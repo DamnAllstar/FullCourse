b4207a4548b35e7511ab021927cad5b70c9d412e
Pae <newscoat109@hotmail.com>
2015-04-03 19:02:55 +0700

    be more login screen and make a link between log in page and profile page

--- app/src/main/java/com/example/chonlaphoom/fullcourse/MainActivity.java
+++ app/src/main/java/com/example/chonlaphoom/fullcourse/MainActivity.java
@@ -4,14 +4,31 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity implements OnClickListener {
+    Button btnStartAnotherActivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_main);
+        btnStartAnotherActivity = (Button) findViewById(R.id.btnStartAnotherActivity);
+
+        btnStartAnotherActivity.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View view) {
+
+        Intent inent = new Intent(this, profile.class);
+
+        // calling an activity using <intent-filter> action name
+        //  Intent inent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
+
+        startActivity(inent);
     }
 
 
