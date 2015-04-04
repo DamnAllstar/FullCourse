38fa345138ac38eb9050fddebb5c5a49d7ecc7ca
Chonlaphoom <ac_krumpinboy@hotmail.com>
2015-04-03 17:52:35 -0700

    with pic

--- app/src/main/java/com/example/chonlaphoom/fullcourse/MainActivity.java
+++ app/src/main/java/com/example/chonlaphoom/fullcourse/MainActivity.java
@@ -4,33 +4,14 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.app.Activity;
-import android.content.Intent;
-import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.content.Intent;
 
-public class MainActivity extends ActionBarActivity implements OnClickListener {
-    Button btnStartAnotherActivity;
+
+public class MainActivity extends ActionBarActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btnStartAnotherActivity = (Button) findViewById(R.id.btnStartAnotherActivity);
-
-        btnStartAnotherActivity.setOnClickListener(this);
-    }
-
-    @Override
-    public void onClick(View view) {
-
-        Intent inent = new Intent(this, profile.class);
-
-        // calling an activity using <intent-filter> action name
-        //  Intent inent = new Intent("com.hmkcode.android.ANOTHER_ACTIVITY");
-
-        startActivity(inent);
+        setContentView(R.layout.activity_profile);
     }
 
 
@@ -56,4 +37,3 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
         return super.onOptionsItemSelected(item);
     }
 }
-
