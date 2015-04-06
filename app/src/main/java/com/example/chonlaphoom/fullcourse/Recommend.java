896d69f0d7ee510fd2c13f4cb6770a030d33c5d5
Chonlaphoom <ac_krumpinboy@hotmail.com>
2015-04-06 12:11:56 -0700

    recoomend done

--- app/src/main/java/com/example/chonlaphoom/fullcourse/Recommend.java
+++ app/src/main/java/com/example/chonlaphoom/fullcourse/Recommend.java
@@ -1,39 +1,32 @@
 package com.example.chonlaphoom.fullcourse;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Recommend extends ActionBarActivity {
+public class Recommend extends ActionBarActivity implements View.OnClickListener {
 
+    Button btnDone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+
+        btnDone = (Button) findViewById(R.id.btnDone);
+        btnDone.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        //click button code here na
+        Intent Newfeed = new Intent(Recommend.this,NewFeed.class);
+        startActivity(Newfeed);
+
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_recommend, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
