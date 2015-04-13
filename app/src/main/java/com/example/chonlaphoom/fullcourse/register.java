c9c43c8bde79074919df9d295a11c39cab1cd4c4
thanatostan <thanatos_tan@hotmail.com>
2015-04-13 18:25:39 +0700

    add google+ login api

--- app/src/main/java/com/example/chonlaphoom/fullcourse/register.java
+++ app/src/main/java/com/example/chonlaphoom/fullcourse/register.java
@@ -1,17 +1,42 @@
 package com.example.chonlaphoom.fullcourse;
 
+import android.content.Context;
+import android.media.Image;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 
 public class register extends ActionBarActivity {
 
+    EditText name;
+    EditText email;
+    EditText password;
+    EditText repassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        ImageButton regist = (ImageButton)findViewById(R.id.imageButton);
+        name = (EditText)findViewById(R.id.editText3);
+        email = (EditText)findViewById(R.id.editText4);
+        password = (EditText)findViewById(R.id.editText5);
+        repassword = (EditText)findViewById(R.id.editText6);
+
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getApplicationContext();
+                Toast.makeText(context,name.getText().toString()+" "+email.getText().toString()+" "+password.getText().toString()
+                +" "+repassword.getText().toString(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
 
