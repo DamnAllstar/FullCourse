b62a15df72886bb5667c025527feaa41775886ca
Chonlaphoom <ac_krumpinboy@hotmail.com>
2015-04-23 09:00:59 -0700

    fetch

---
+++ app/src/main/java/com/example/chonlaphoom/fullcourse/ApiConnector.java
@@ -0,0 +1,78 @@
+package com.example.chonlaphoom.fullcourse;
+
+import android.util.Log;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+
+/**
+ * Created by Chonlaphoom on 4/23/2015.
+ */
+public class ApiConnector {
+
+
+    public JSONArray GetAllcustomer()
+    {
+        String url = "http://naneport.arg.in.th/eatwell/full/";
+         HttpEntity httpEntity = null;
+
+      try
+      {
+
+         DefaultHttpClient httpClient = new DefaultHttpClient();  // Default HttpClient
+          HttpGet httpGet = new HttpGet(url);
+
+          HttpResponse httpResponse = httpClient.execute(httpGet);
+
+          httpEntity = httpResponse.getEntity();
+
+
+
+       } catch (ClientProtocolException e) {
+
+            // Signals error in http protocol
+         e.printStackTrace();
+
+            //Log Errors Here
+
+
+
+     } catch (IOException e) {
+          e.printStackTrace();
+      }
+
+        // Convert HttpEntity into JSON Array
+        JSONArray jsonArray = null;
+
+        if (httpEntity != null) {
+            try {
+                String entityResponse = EntityUtils.toString(httpEntity);
+
+                Log.e("Entity Response  : ", entityResponse);
+
+                jsonArray = new JSONArray(entityResponse);
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return jsonArray;
+
+
+    }
+
+
+
+    }
+
--- app/src/main/java/com/example/chonlaphoom/fullcourse/MainActivity.java
+++ app/src/main/java/com/example/chonlaphoom/fullcourse/MainActivity.java
@@ -1,17 +1,19 @@
 package com.example.chonlaphoom.fullcourse;
 
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.content.Intent;
 import android.view.View;
+
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -54,8 +56,8 @@ public class MainActivity extends ActionBarActivity {
                 Toast.makeText(context, email.getText().toString()+" "+password.getText().toString(), Toast.LENGTH_SHORT).show();
 
                 //go to recommend
-                //Intent intent = new Intent(MainActivity.this, Recommend.class);
-                //startActivity(intent);
+                Intent intent = new Intent(MainActivity.this, NewFeeds.class);
+                startActivity(intent);
             }
         });
         btn2.setOnClickListener(new View.OnClickListener() {
@@ -86,7 +88,7 @@ public class MainActivity extends ActionBarActivity {
 
                 intent.putExtra("Arl",LL);  //Array passing
                 intent.putExtra("type",0);  //use type 1 to use a one place marking function
-                                            //type 0 to use many place marking function !!assign in the array above
+                //type 0 to use many place marking function !!assign in the array above
                 //Type 1 passing variable
                 intent.putExtra("Lat",18.769289);
                 intent.putExtra("Lng",98.976250);
@@ -96,8 +98,6 @@ public class MainActivity extends ActionBarActivity {
             }
         });
     }
-
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
